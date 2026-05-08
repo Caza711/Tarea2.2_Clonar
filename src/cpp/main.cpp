b37fa4acf7f47e0abc1e38f2c90ac64b0ddec1af
Caza711 <jojeda2222@uta.edu.ec>
2026-05-07 20:19:56 -0500

    Update main.cpp
    Agrego los 5 nodos nuevos

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -2,6 +2,7 @@
 #include <queue>
 using namespace std;
 
+// Nodo del arbol binario
 struct Nodo {
     int dato;
     Nodo* izquierda;
@@ -14,6 +15,7 @@ struct Nodo {
     }
 };
 
+// Raiz -> Izquierda -> Derecha
 void preorden(Nodo* raiz) {
     if (raiz == nullptr) return;
     cout << raiz->dato << " ";
@@ -21,6 +23,7 @@ void preorden(Nodo* raiz) {
     preorden(raiz->derecha);
 }
 
+// Izquierda -> Raiz -> Derecha (orden ascendente en BST)
 void inorden(Nodo* raiz) {
     if (raiz == nullptr) return;
     inorden(raiz->izquierda);
@@ -28,6 +31,7 @@ void inorden(Nodo* raiz) {
     inorden(raiz->derecha);
 }
 
+// Izquierda -> Derecha -> Raiz
 void postorden(Nodo* raiz) {
     if (raiz == nullptr) return;
     postorden(raiz->izquierda);
@@ -35,46 +39,45 @@ void postorden(Nodo* raiz) {
     cout << raiz->dato << " ";
 }
 
+// Recorrido por niveles usando cola
 void bfs(Nodo* raiz) {
     if (raiz == nullptr) return;
-
     queue<Nodo*> cola;
     cola.push(raiz);
-
     while (!cola.empty()) {
         Nodo* actual = cola.front();
         cola.pop();
-
         cout << actual->dato << " ";
-
         if (actual->izquierda != nullptr) cola.push(actual->izquierda);
-        if (actual->derecha != nullptr) cola.push(actual->derecha);
+        if (actual->derecha   != nullptr) cola.push(actual->derecha);
     }
 }
 
 int main() {
-    Nodo* raiz = new Nodo(10);
-    raiz->izquierda = new Nodo(5);
-    raiz->derecha = new Nodo(15);
-    raiz->izquierda->izquierda = new Nodo(2);
-    raiz->izquierda->derecha = new Nodo(7);
-    raiz->derecha->izquierda = new Nodo(12);
-    raiz->derecha->derecha = new Nodo(20);
+    // Nodos base del repositorio
+    Nodo* raiz = new Nodo(50);
+    raiz->izquierda = new Nodo(30);
+    raiz->derecha   = new Nodo(70);
+    raiz->izquierda->izquierda = new Nodo(20);
+    raiz->izquierda->derecha   = new Nodo(40);
+    raiz->derecha->izquierda   = new Nodo(60);
+    raiz->derecha->derecha     = new Nodo(80);
 
-    cout << "RECORRIDOS DE ARBOLES BINARIOS - UTA" << endl;
+    // Nodos nuevos agregados (6 nodos)
+    raiz->izquierda->izquierda->izquierda        = new Nodo(10);
+    raiz->izquierda->izquierda->derecha          = new Nodo(25);
+    raiz->izquierda->izquierda->derecha->derecha = new Nodo(28);
+    raiz->derecha->izquierda->derecha            = new Nodo(65);
+    raiz->derecha->derecha->derecha              = new Nodo(90);
+    raiz->derecha->derecha->derecha->derecha     = new Nodo(95);
 
-    cout << "Preorden: ";
-    preorden(raiz);
+    cout << "=== RECORRIDOS DE ARBOLES BINARIOS - UTA ===" << endl;
 
-    cout << "\nInorden: ";
-    inorden(raiz);
-
-    cout << "\nPostorden: ";
-    postorden(raiz);
-
-    cout << "\nBFS: ";
-    bfs(raiz);
-
+    cout << "Preorden  : "; preorden(raiz);
+    cout << "\nInorden   : "; inorden(raiz);
+    cout << "\nPostorden : "; postorden(raiz);
+    cout << "\nBFS       : "; bfs(raiz);
     cout << endl;
+
     return 0;
 }
